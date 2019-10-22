43c16daccea7bed2d77af73d6b6a1c6514c6ba12
ch00486259 <ch00486259@126.com>
2019-10-22 10:18:03 +0800

    rename assembly

--- src/Socean.Rpc/Properties/AssemblyInfo.cs
+++ src/Socean.Rpc/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Socean.Rpc.Core")]
+[assembly: AssemblyTitle("Socean.Rpc")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Socean")]
-[assembly: AssemblyProduct("Socean.Rpc.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Socean.Rpc")]
+[assembly: AssemblyCopyright("Copyright © 2019 Socean")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.2.0")]
-[assembly: AssemblyFileVersion("0.0.2.0")]
+[assembly: AssemblyVersion("0.0.2.1")]
+[assembly: AssemblyFileVersion("0.0.2.1")]
